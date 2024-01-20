2fd362b958e8e877451084c6493c5a6b9d1a3656
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2024-01-20 17:54:19 +0900

    fix: indentation

--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,53 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  //
+  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+  // MY_SERVICE: Fetcher;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		async function MethodNotAllowed(request: Request) {
-			return new Response(`Method ${request.method} not allowed.`, {
-				status: 405,
-				headers: {
-					Allow: "GET",
-				},
-			});
-		}
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    async function MethodNotAllowed(request: Request) {
+      return new Response(`Method ${request.method} not allowed.`, {
+        status: 405,
+        headers: {
+          Allow: "GET",
+        },
+      });
+    }
 
-		if (request.method !== "GET") return MethodNotAllowed(request);
+    if (request.method !== "GET") return MethodNotAllowed(request);
 
-		const response =
-			await fetch("https://cdn.jsdelivr.net/gh/MalitsPlus/ShizuruNotes@test/update_log.json", {
-				method: "GET",
-				headers: {
-					"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-					"Accept-Encoding": "gzip, deflate, br",
-					"Cache-Control": "no-cache",
-				},
-			})
-		const resText = await response.text()
+    const response =
+      await fetch("https://cdn.jsdelivr.net/gh/MalitsPlus/ShizuruNotes@test/update_log.json", {
+        method: "GET",
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Cache-Control": "no-cache",
+        },
+      })
+    const resText = await response.text()
 
-		return new Response(resText, {
-			status: 200,
-			headers: {
-				"Content-Type": "application/json; charset=utf-8",
-				"Cache-Control": "no-cache",
-			}
-		})
-	},
+    return new Response(resText, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Cache-Control": "no-cache",
+      }
+    })
+  },
 };
